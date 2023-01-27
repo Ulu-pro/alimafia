762fb53b8576f308bd433eed469791d5a142d384
Ulu-pro <web.ulu7@gmail.com>
2023-01-27 21:48:24 +0500

    Index Page

--- index.php
+++ index.php
@@ -1,2 +1,9 @@
 <?php
-echo "Ali Mafia";
+require "db.php";
+$db = new DB();
+
+$db->select(Tables::$CATEGORIES, function ($category) {
+  $id = $category["id"];
+  $title = $category["title"];
+  echo "$id: $title<br>";
+});
