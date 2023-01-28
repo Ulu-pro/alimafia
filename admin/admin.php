e5d4cadcf16f9a96631a799b652b82359f6e14d6
Ulu-pro <web.ulu7@gmail.com>
2023-01-28 19:08:04 +0500

    Admin: added DataBase manager

--- admin/admin.php
+++ admin/admin.php
@@ -8,7 +8,11 @@
   <link rel="stylesheet" href="/static/bootstrap.min.css">
 </head>
 <body data-bs-theme="dark">
-<?php require "modals.php"; ?>
+<?php
+require "../db.php";
+require "modals.php";
+$db = new DB();
+?>
 <div class="container mt-2">
   <div class="row">
     <div class="col-12">
@@ -47,9 +51,6 @@
       </div>
   </div>
 </div>
-<?php
-
-?>
 <script src="/static/bootstrap.bundle.min.js"></script>
 </body>
 </html>
