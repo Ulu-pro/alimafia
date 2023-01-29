115ec822d407b7cd1b405f70f19dd2480e9fbcc1
Ulu-pro <web.ulu7@gmail.com>
2023-01-29 15:07:07 +0500

    Updated "INSERT" and added "UPDATE", "DELETE" + escape for element's value field

--- db.php
+++ db.php
@@ -11,7 +11,11 @@ class DB {
   }
 
   public function find($table, $row_id) {
-    return $this->db->query("SELECT * FROM $table WHERE id = $row_id")->fetch_assoc();
+    $result = $this->db->query("SELECT * FROM $table WHERE id = $row_id")->fetch_assoc();
+    foreach($result as $key => $value) {
+      $result[$key] = htmlspecialchars($value);
+    }
+    return $result;
   }
 
   public function select($table, $callback) {
@@ -25,16 +29,27 @@ class DB {
     $keys = [];
     $values = [];
     foreach ($options as $key => $value) {
+      $point = $this->escape($value);
       $keys[] = $this->escape($key);
-      $values[] = $this->escape($value);
-    }
-    $data = [];
-    foreach ($values as $value) {
-      $data[] = is_numeric($value) ? $value : "'$value'";
+      $values[] = is_numeric($point) ? $point : "'$point'";
     }
     $keys = implode(",", $keys);
+    $values = implode(",", $values);
+    $this->db->query("INSERT INTO $table ($keys) VALUES ($values)");
+  }
+
+  public function update($table, $row_id, $options) {
+    $data = [];
+    foreach ($options as $key => $value) {
+      $point = $this->escape($value);
+      $data[] = $this->escape($key) . "=" . (is_numeric($point) ? $point : "'$point'");
+    }
     $data = implode(",", $data);
-    $this->db->query("INSERT INTO $table ($keys) VALUES ($data)");
+    $this->db->query("UPDATE $table SET $data WHERE id = $row_id");
+  }
+
+  public function delete($table, $row_id) {
+    $this->db->query("DELETE FROM $table WHERE id = $row_id");
   }
 }
 
