b8b09c5621f338a4efedd15b5919efb4b04ea4e3
Ulu-pro <web.ulu7@gmail.com>
2023-01-29 12:10:28 +0500

    GET params instead of hidden input ID + delete param when modal closed

--- static/modal.data.js
+++ static/modal.data.js
@@ -2,6 +2,20 @@ for (let element of document.querySelectorAll(".modal-data")) {
   element.onclick = function (event) {
     let id = event.target.getAttribute("data-row-id")
     let flag = event.target.getAttribute("data-modal-flag")
-    document.getElementById(flag + "RowID").value = id;
+    document.location.search = flag + "=" + id
   }
+}
+
+for (let element of document.querySelectorAll(".modal")) {
+  element.addEventListener("hidden.bs.modal", function () {
+    document.location.search = ""
+  })
+}
+
+if (document.location.search !== "") {
+  let action = document.location.search
+      .substring(1)
+      .split("&")[0]
+      .split("=")[0]
+  document.getElementById(action + "_modal").click()
 }
