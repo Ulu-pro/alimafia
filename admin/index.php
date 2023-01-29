f4f81c2f1aec8ff72b4aa1898e651f48ce345ada
Ulu-pro <web.ulu7@gmail.com>
2023-01-29 15:11:09 +0500

    Admin POST handler: log in, log out, create, edit, delete

--- admin/index.php
+++ admin/index.php
@@ -8,21 +8,44 @@ $tables = [
 
 if (!empty($_POST)) {
   $location = "admin";
+
   if (isset($_POST["login"])) {
     if ($_POST["account"] == "admin" &&
         $_POST["password"] == "admin") {
       $_SESSION["admin"] = "";
     }
-  } else if (isset($_POST["logout"])) {
+  }
+
+  else if (isset($_POST["logout"])) {
     session_destroy();
     $location = "";
-  } else if (isset($_POST["create"])) {
+  }
+
+  else if (isset($_POST["create"])) {
     if (in_array($_POST["create"], $tables)) {
       $data = $_POST;
       unset($data["create"]);
       $db->insert($_POST["create"], $data);
     }
   }
+
+  else if (isset($_POST["edit"])) {
+    if (in_array($_POST["edit"], $tables)) {
+      $data = $_POST;
+      $id = $data["id"];
+      unset($data["id"]);
+      unset($data["edit"]);
+      $db->update($_POST["edit"], $id, $data);
+    }
+  }
+
+  else if (isset($_POST["delete"])) {
+    if (in_array($_POST["delete"], $tables)) {
+      $id = $_POST["id"];
+      $db->delete($_POST["delete"], $id);
+    }
+  }
+
   header("Location: /$location");
 }
 
