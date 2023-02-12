97754b037a4a2bdf4d3158e0ac54b5e072bad95d
Ulu-pro <web.ulu7@gmail.com>
2023-02-12 16:26:00 +0500

    File upload handler for tables

--- admin/index.php
+++ admin/index.php
@@ -22,32 +22,54 @@ if (!empty($_POST)) {
     $location = "";
   }
 
-  else if (isset($_POST["create"])) {
-    if (in_array($_POST["create"], $tables)) {
+  else if (isset($_POST["create"]) || isset($_POST["edit"]) || isset($_POST["delete"])) {
+    if (in_array($_POST["create"] ?? $_POST["edit"] ?? $_POST["delete"], $tables)) {
       $data = $_POST;
-      unset($data["create"]);
-      $db->insert($_POST["create"], $data);
-    }
-  }
-
-  else if (isset($_POST["edit"])) {
-    if (in_array($_POST["edit"], $tables)) {
-      $data = $_POST;
-      $id = $db->escape($data["id"]);
+      $id = $db->escape($data["id"] ?? null);
       unset($data["id"]);
+      unset($data["create"]);
       unset($data["edit"]);
-      $db->update($_POST["edit"], $id, $data);
-    }
-  }
-
-  else if (isset($_POST["delete"])) {
-    if (in_array($_POST["delete"], $tables)) {
-      $id = $db->escape($_POST["id"]);
-      $db->delete($_POST["delete"], $id);
+      unset($data["delete"]);
+      $mode = false;
+      if (isset($_POST["create"])) {
+        $db->insert($_POST["create"], $data);
+      } else if (isset($_POST["edit"])) {
+        $db->update($_POST["edit"], $id, $data);
+      } else if (isset($_POST["delete"])) {
+        $db->delete($_POST["delete"], $id);
+        $mode = true;
+      }
+      if (isset($_FILES["image"]) and $_FILES["image"]["error"] == false) {
+        file_handler($id, $_FILES["image"], $mode);
+      }
     }
   }
 
   header("Location: /$location");
+}
+
+function file_handler($id, $image, $mode) {
+  $size = $image["size"];
+  $limit = 10 * (2 ** 20);
+  $temporary = $image["tmp_name"];
+  $allowed = ["png", "jpg", "jpeg", "bmp", "gif"];
+  $default = "jpg";
+  $extension = pathinfo($image["name"], PATHINFO_EXTENSION);
+  $directory = $_SERVER["DOCUMENT_ROOT"]."/media";
+  $file = "$directory/$id.";
+  $path = $file.in_array($extension, $allowed) ? $extension : $default;
+
+  if (!is_dir($directory)) mkdir($directory);
+  if (file_exists($file.$default)) unlink($file.$default);
+  if ($size < 0 or $size > $limit or $mode) return;
+
+  move_uploaded_file($temporary, $path);
+  $content = file_get_contents($path);
+  $convert = imagecreatefromstring($content);
+  imagejpeg($convert, $file.$default);
+  imagedestroy($convert);
+
+  if (file_exists($extension)) unlink($extension);
 }
 
 if (isset($_SESSION["admin"])) {
